65ce7c5b1a6f433c8edd34a4c75fa0f49e2e6e0a
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-02-14 17:45:55 -0500

    Add forward movement in the direction the player is facing

--- Assets/Scripts/MainGameScript/3D/FirstPersonController.cs
+++ Assets/Scripts/MainGameScript/3D/FirstPersonController.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class FirstPersonController : MonoBehaviour
@@ -9,24 +11,46 @@ public class FirstPersonController : MonoBehaviour
     public float moveSpeed = 5f;
     [Tooltip("Sprint speed of the player in m/s")]
     public float sprintSpeed = 15f;
+    [Tooltip("Rotation speed of the character")]
+    public float mouseSensitivity = 0.1f;
+    
+    [Header("Cinemachine")]
+    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+    public GameObject CinemachineCameraTarget;
+    [Tooltip("How far in degrees can you move the camera up")]
+    public float TopClamp = 90.0f;
+    [Tooltip("How far in degrees can you move the camera down")]
+    public float BottomClamp = -90.0f;
+    
+    float _cinemachineTargetPitch;
 
     float _speed;
     float _verticalVelocity;
+    float _rotationVelocity;
 
+    private PlayerInput _playerInput;
     CharacterController _controller;
     FirstPersonInputs _input;
+    
+    const float _threshold = 0.01f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _controller = GetComponent<CharacterController>();
         _input = GetComponent<FirstPersonInputs>();
+        _playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraRotation();
     }
 
     void Move()
@@ -45,5 +69,30 @@ public class FirstPersonController : MonoBehaviour
         }
         
         _controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+    }
+
+    void CameraRotation()
+    {
+        if (_input.look.sqrMagnitude >= _threshold)
+        {
+            float mouseX = _input.look.x * mouseSensitivity;
+            float mouseY = _input.look.y * mouseSensitivity;
+            
+            _cinemachineTargetPitch += mouseY;
+            _rotationVelocity = mouseX;
+            
+            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+            
+            CinemachineCameraTarget.transform.localRotation = Quaternion.Euler(_cinemachineTargetPitch, 0.0f, 0.0f);
+            
+            transform.Rotate(Vector3.up * _rotationVelocity);
+        }
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f) angle += 360f;
+        if (angle > 360f) angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
     }
 }
--- Assets/Scripts/MainGameScript/3D/FirstPersonInputs.cs
+++ Assets/Scripts/MainGameScript/3D/FirstPersonInputs.cs
@@ -12,16 +12,24 @@ public class FirstPersonInputs : MonoBehaviour
 {
     [Header("Player Input Values")]
     public Vector2 move;
+    public Vector2 look;
     public bool sprint;
     
     [Header("Mouse Cursor Settings")]
     public bool cursorLocked = true;
+    public bool cursorIsInputForLook = true;
 
     public void OnMove(InputValue value) => MoveInput(value.Get<Vector2>());
     public void OnSprint(InputValue value) => SprintInput(value.isPressed);
     public void OnApplicationFocus(bool hasFocus) =>  SetCursorState(hasFocus);
-
+    public void OnLook(InputValue value)
+    {
+        if (cursorIsInputForLook) LookInput(value.Get<Vector2>());
+    }
     public void MoveInput(Vector2 newMoveDirection) => move = newMoveDirection;
+    public void LookInput(Vector2 newLookDirection) => look = newLookDirection;
+
+
     public void SprintInput(bool newSprintState) => sprint = newSprintState;
     private void SetCursorState(bool newState)
     {
