1714d8757628cf9a1b2c97e3ba41886405de433f
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-02-16 10:24:58 -0500

    First person movement
    * Add floor to scene
    
    Installed "Pro Builder" package from the Unity Registry
    
    * Add first person view
    
    Installed Cinemachine 3.1.5 package
    Configured the head for a first person view
    
    * Add cursor lock when playing the game in first person
    
    Add the CursorLock script
    Update scene to move the floor to the center as well as the player
    
    * Sus
    
    * Add simple movement
    
    * Add forward movement in the direction the player is facing
    
    * Minor text adjustments
    
    * Add ability to jump for the player
    
    * Sus
    
    * Add simple platform elements for test and fix first person camera issue
    
    The camera rotation had calibration issues with the mouse sometimes
    
    * Add player to the main game scene
    
    * Add basic readme
    
    * adjust name

---
+++ Assets/Scripts/MainGameScript/3D/FirstPersonController.cs
@@ -0,0 +1,159 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class FirstPersonController : MonoBehaviour
+{
+    [FormerlySerializedAs("MoveSpeed")]
+    [Header("Player")]
+    [Tooltip("Movement speed of the player in m/s")]
+    public float moveSpeed = 5f;
+    [Tooltip("Sprint speed of the player in m/s")]
+    public float sprintSpeed = 15f;
+    [Tooltip("Rotation speed of the character")]
+    public float mouseSensitivity = 0.1f;
+    
+    [Space(10)]
+    [Tooltip("The height the player can jump")]
+    public float jumpHeight = 1.2f;
+    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+    public float gravity = -15f;
+    
+    [Space(10)]
+    [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+    public float jumpTimeout = 0.1f;
+    [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+    public float fallTimeout = 0.15f;
+    
+    [Header("Player Grounded")]
+    [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
+    public bool grounded = true;
+    [Tooltip("Useful for rough ground")]
+    public float groundedOffset = 0.7f;
+    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+    public float groundedRadius = 0.5f;
+    [Tooltip("What layers the character uses as ground")]
+    public LayerMask groundLayers;
+    
+    [Header("Cinemachine")]
+    [Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+    public GameObject cinemachineCameraTarget;
+    [Tooltip("How far in degrees can you move the camera up")]
+    public float topClamp = 90.0f;
+    [Tooltip("How far in degrees can you move the camera down")]
+    public float bottomClamp = -90.0f;
+    
+    float _cinemachineTargetPitch;
+
+    float _speed;
+    float _verticalVelocity;
+    float _rotationVelocity;
+    float _terminalVelocity = 53.0f;
+    
+    private float _jumpTimeoutDelta;
+    private float _fallTimeoutDelta;
+
+    private PlayerInput _playerInput;
+    CharacterController _controller;
+    FirstPersonInputs _input;
+    
+    const float Threshold = 0.01f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _input = GetComponent<FirstPersonInputs>();
+        _playerInput = GetComponent<PlayerInput>();
+
+        _jumpTimeoutDelta = jumpTimeout;
+        _fallTimeoutDelta = fallTimeout;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundedCheck();
+        JumpWithGravity();
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    void Move()
+    {
+        float targetSpeed = _input.sprint ? sprintSpeed : moveSpeed;
+        
+        if (_input.move == Vector2.zero) targetSpeed = 0.0f;
+        
+        _speed = targetSpeed;
+        
+        Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+        if (_input.move != Vector2.zero)
+        {
+            inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
+        }
+        
+        _controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+    }
+
+    private void JumpWithGravity()
+    {
+        if (grounded)
+        {
+            _fallTimeoutDelta = fallTimeout;
+
+            if (_verticalVelocity < 0.0f) _verticalVelocity = -2f;
+            if (_input.jump && _jumpTimeoutDelta <= 0.0f) _verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            if (_jumpTimeoutDelta >= 0.0f) _jumpTimeoutDelta -= Time.deltaTime;
+        }
+        else
+        {
+            _jumpTimeoutDelta = jumpTimeout;
+            
+            if (_fallTimeoutDelta >= 0.0f) _fallTimeoutDelta -= Time.deltaTime;
+            
+            _input.jump = false;
+        }
+        
+        if (_verticalVelocity < _terminalVelocity) _verticalVelocity += gravity * Time.deltaTime;
+    }
+
+    void GroundedCheck()
+    {
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z);
+        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
+    }
+
+    private float _cinemachineTargetYaw;
+
+    void CameraRotation()
+    {
+        if (_input.look.sqrMagnitude >= Threshold)
+        {
+            float mouseX = _input.look.x * mouseSensitivity;
+            float mouseY = _input.look.y * mouseSensitivity;
+            
+            _cinemachineTargetPitch -= mouseY; 
+            _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, bottomClamp, topClamp);
+            
+            _cinemachineTargetYaw += mouseX;
+            
+            cinemachineCameraTarget.transform.localRotation = Quaternion.Euler(_cinemachineTargetPitch, 0.0f, 0.0f);
+            
+            transform.rotation = Quaternion.Euler(0.0f, _cinemachineTargetYaw, 0.0f);
+        }
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f) angle += 360f;
+        if (angle > 360f) angle -= 360f;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/Scripts/MainGameScript/3D/FirstPersonInputs.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/**
+ * This class serves as a bridge between the Input System and a control logic (e.g. Player movement).
+ * It's main use is to store the current state of commands (e.g. Is the player pressing Jump right now?)
+ * so other scripts can read them.
+ * 
+ * Source of inspiration: https://assetstore.unity.com/packages/essentials/starter-assets-firstperson-updates-in-new-charactercontroller-pa-196525#content
+ */
+public class FirstPersonInputs : MonoBehaviour
+{
+    [Header("Player Input Values")]
+    public Vector2 move;
+    public Vector2 look;
+    public bool sprint;
+    public bool jump;
+    
+    [Header("Mouse Cursor Settings")]
+    public bool cursorLocked = true;
+    public bool cursorIsInputForLook = true;
+
+    public void OnMove(InputValue value) => MoveInput(value.Get<Vector2>());
+    public void OnSprint(InputValue value) => SprintInput(value.isPressed);
+    public void OnJump(InputValue value) => JumpInput(value.isPressed);
+    public void OnApplicationFocus(bool hasFocus) =>  SetCursorState(hasFocus);
+    public void OnLook(InputValue value)
+    {
+        if (cursorIsInputForLook) LookInput(value.Get<Vector2>());
+    }
+
+    public void MoveInput(Vector2 newMoveDirection) => move = newMoveDirection;
+    public void LookInput(Vector2 newLookDirection) => look = newLookDirection;
+    public void SprintInput(bool newSprintState) => sprint = newSprintState;
+    public void JumpInput(bool newJumpState) => jump = newJumpState;
+    private void SetCursorState(bool newState)
+    {
+        bool shouldLock = newState && cursorLocked;
+        Cursor.lockState = shouldLock ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = !newState;
+    }
+}
