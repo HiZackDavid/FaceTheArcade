e4c445d8c93b3be5e6986d1f181e62832aef7a28
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-03-10 20:13:39 -0400

    Minigame lifecycle (#19)
    * Update scripts to implement the minigame interface
    
    * Created an interface to help manage the Game's lifecycle
    
    * Update Legend of Adlez for better quality of life
    
    * reorganize files and folders

---
+++ Assets/Scripts/MainGameScript/MiniGames/Legend of Adlez/LoAMinigame.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class LoAMinigame : MonoBehaviour, IMinigame
+{
+    [Header("Characters References")]
+    [SerializeField] Transform player;
+    [SerializeField] Transform anomaly;
+    
+    [Header("Spawn Points")]
+    [SerializeField] Transform playerSpawnPoint;
+    [SerializeField] Transform anomalySpawnPoint;
+    
+    [Header("Gameplay References")]
+    [SerializeField] private CharacterHealthScript playerHealth;
+    [SerializeField] private CharacterHealthScript anomalyHealth;
+
+    [Header("Minigame Wrapper")]
+    [SerializeField] GameObject container;
+
+    private void Update()
+    {
+        CheckHealth();
+    }
+
+    public void StartGame()
+    {
+        ResetGame();
+        SetGameplayEnabledState(true);
+    }
+
+    public void ResetGame()
+    {
+        ResetGameState();
+        SetGameplayEnabledState(true);
+    }
+
+    public void EndGame()
+    {
+        SetGameplayEnabledState(false);
+        CameraManager.instance.SwitchToPrimaryCamera();
+    }
+
+    void SetGameplayEnabledState(bool isEnabled)
+    {
+        if (container != null) container.SetActive(isEnabled);
+    }
+
+    void ResetGameState()
+    {
+        if (player != null && playerSpawnPoint != null) player.position = playerSpawnPoint.position;
+        if (anomaly != null && anomalySpawnPoint != null) anomaly.position = anomalySpawnPoint.position;
+    }
+
+    void CheckHealth()
+    {
+        if (playerHealth != null && playerHealth.IsDead() 
+            || (anomalyHealth != null && anomalyHealth.IsDead()))
+        {
+            EndGame();
+        }
+    }
+}
