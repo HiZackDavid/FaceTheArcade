c5bde02ce696cc5aa7d962001ee0862feeb88f42
Nass_ <ibendrissou03@gmail.com>
2026-03-13 11:06:14 -0400

    Copy Maingame scene to my dev folder

---
+++ Assets/GameplayAreaRed.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameplayAreaRed : MonoBehaviour, IMinigame
+{
+    public void EndGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ResetGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator ShutDownMinigameAfterDelay()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
