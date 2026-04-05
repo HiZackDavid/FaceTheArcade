7d48b2b07ad5b170b30a9ccbcaeb130364736e16
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2026-04-05 17:20:46 -0400

    Opacity flicker when taking damage

--- Assets/Scripts/MainGameScript/MiniGames/Common/CharacterHealthScript.cs
+++ Assets/Scripts/MainGameScript/MiniGames/Common/CharacterHealthScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class CharacterHealthScript : MonoBehaviour
@@ -9,6 +10,15 @@ public class CharacterHealthScript : MonoBehaviour
     [Header("Audio")]
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip[] hurtClips;
+    
+    [Header("Damage Flicker")]
+    [SerializeField] private SpriteRenderer[] SpriteRenderers;
+    [SerializeField] private float flickerDuration = 0.2f;
+    [SerializeField] private float flickerInterval = 0.05f;
+    [SerializeField] [Range(0f, 1f)] private float lowAlpha = 0.25f;
+    
+    bool _isFlickering = false;
+    private int _lastPlayedIndex = -1;
 
     public event Action<float> OnHealthChanged;
 
@@ -27,6 +37,40 @@ public class CharacterHealthScript : MonoBehaviour
         audioSource.PlayOneShot(clip);
     }
 
+    IEnumerator FlickerEntity()
+    {
+        if (SpriteRenderers == null || SpriteRenderers.Length == 0) yield break;
+        
+        _isFlickering = true;
+        
+        float elapsedTime = 0f;
+        bool low = true;
+
+        while (elapsedTime < flickerInterval)
+        {
+            SetAlpha(low ? lowAlpha : 1f);
+            low = !low;
+            
+            yield return new WaitForSeconds(flickerInterval);
+            elapsedTime += flickerInterval;
+        }
+        
+        SetAlpha(1f);
+        _isFlickering = false;
+    }
+
+    void SetAlpha(float alpha)
+    {
+        foreach (SpriteRenderer sr in SpriteRenderers)
+        {
+            if (!sr) continue;
+            
+            Color c = sr.color;
+            c.a = alpha;
+            sr.color = c;
+        }
+    }
+
     public void TakeDamage(float damageAmount)
     {
         _currentHealth -= damageAmount;
@@ -34,6 +78,11 @@ public class CharacterHealthScript : MonoBehaviour
         OnHealthChanged?.Invoke(_currentHealth / _maxHealth * 100);
         
         PlayRandomHurtClip();
+
+        if (!_isFlickering)
+        {
+            StartCoroutine(FlickerEntity());
+        }
     }
 
     public bool IsDead()
